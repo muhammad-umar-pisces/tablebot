39d4557159f45c2323eff95bd3d1da1de453569e
umar <umar032190@gmail.com>
2022-09-18 20:08:32 +0500

    update in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Robot.create(name: "test robot")
-BotPosition.create(x_axis: 0, y_axis: 0, robot_id: 1)
+BotPosition.create(x_axis: 0, y_axis: 0, robot_id: Robot.last.id)
