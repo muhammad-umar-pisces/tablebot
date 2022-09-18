ec314aca18747cfd92f4f3f64781d7b8e63b9341
umar <umar032190@gmail.com>
2022-09-18 19:22:59 +0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :robots
+  root 'robots#show'
+  post 'bot_positions/place'
+  post 'bot_positions/right'
+  post 'bot_positions/left'
+  post 'bot_positions/up'
+  post 'bot_positions/down'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
